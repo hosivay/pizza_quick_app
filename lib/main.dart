430389725c53125e077d035bb29383fe1e2d640e
hosivay <h.v.b1383@gmail.com>
2023-08-28 09:49:03 +0330

    added Pizza Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pizza Quick',
       debugShowCheckedModeBanner: false,
-      theme: FlexThemeData.light(scheme: FlexScheme.gold,useMaterial3: true), 
-      darkTheme: FlexThemeData.dark(scheme: FlexScheme.gold,useMaterial3: true), 
+      theme: FlexThemeData.light(scheme: FlexScheme.gold, useMaterial3: true),
+      darkTheme:
+          FlexThemeData.dark(scheme: FlexScheme.gold, useMaterial3: true),
       themeMode: ThemeMode.system,
       home: const MyHomePage(),
     );
   }
 }
 
- class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Image.asset("lib/assets/Pepperoni Pizza.png"),
+    ));
   }
 }
